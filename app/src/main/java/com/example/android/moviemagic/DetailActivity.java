3180302b225e5ed55e8c12c08bdce8ce1fe3a05b
pkennedy89 <patrickken@gmail.com>
2018-01-22 14:49:07 +0000

    created detail view

--- app/src/main/java/com/example/android/moviemagic/DetailActivity.java
+++ app/src/main/java/com/example/android/moviemagic/DetailActivity.java
@@ -1,20 +1,48 @@
 package com.example.android.moviemagic;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ImageView mPoster;
+        TextView mTitle, mUserRating, mReleaseDate, mOverview;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         Intent intentThatStartedThisActivity = getIntent();
         Movie movie = (Movie) intentThatStartedThisActivity.getSerializableExtra("movie");
-        Log.v("DetailActivity", "Movie Title: " + movie.getmTitle());
 
+        /* Find the image and text views in the Detail Activity*/
+        mPoster = (ImageView) findViewById(R.id.iv_poster);
+        mTitle = (TextView) findViewById(R.id.tv_title);
+        mUserRating = (TextView) findViewById(R.id.tv_rating);
+        mReleaseDate = (TextView) findViewById(R.id.tv_release_date);
+        mOverview = (TextView) findViewById(R.id.tv_overview);
+
+        /* Bind the data to the views above */
+
+        Picasso.with(this)
+                .load(movie.getmPosterPath())
+                .into(mPoster);
+
+        mTitle.setText(movie.getmTitle());
+        mUserRating.setText(movie.getmRating());
+        mReleaseDate.setText(movie.getmReleaseDate());
+        mOverview.setText(movie.getmSynopsis());
     }
 }
