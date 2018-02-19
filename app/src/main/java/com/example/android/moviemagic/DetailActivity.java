df3d10cd3cca5ebbe07d0fc12c656c21a4485021
pkennedy89 <patrickken@gmail.com>
2018-02-19 19:51:35 +0000

    testing trailers

--- app/src/main/java/com/example/android/moviemagic/DetailActivity.java
+++ app/src/main/java/com/example/android/moviemagic/DetailActivity.java
@@ -1,16 +1,34 @@
 package com.example.android.moviemagic;
 
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.android.moviemagic.utilities.MovieJsonUtils;
+import com.example.android.moviemagic.utilities.NetworkUtils;
 import com.squareup.picasso.Picasso;
 
-public class DetailActivity extends AppCompatActivity {
+import java.net.URL;
+import java.util.ArrayList;
+
+public class DetailActivity extends AppCompatActivity implements TrailerAdapter.TrailerAdapterOnClickHandler{
 
     private String mId;
+
+    private RecyclerView mTrailersRecyclerView;
+
+    private LinearLayoutManager mTrailersLayoutManager;
+
+    private TrailerAdapter mTrailerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,5 +61,73 @@ public class DetailActivity extends AppCompatActivity {
         mUserRating.setText(movie.getmRating());
         mReleaseDate.setText(movie.getmReleaseDate());
         mOverview.setText(movie.getmSynopsis());
+
+        /* Setup the Trailers Recyclerview */
+        mTrailersRecyclerView = (RecyclerView) findViewById(R.id.rv_trailers);
+
+        mTrailersLayoutManager = new LinearLayoutManager(DetailActivity.this);
+
+        mTrailersRecyclerView.setHasFixedSize(true);
+
+        mTrailersRecyclerView.setLayoutManager(mTrailersLayoutManager);
+
+        mTrailerAdapter = new TrailerAdapter(this, this);
+
+        /* Fetch the trailer data from the API */
+        loadTrailerData();
+
     }
+
+    @Override
+    public void onClick(Trailer trailer) {
+        Context context = this;
+        Intent appIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:" + trailer.getmKey()));
+        Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("http://www.youtube.com/watch?v=" + trailer.getmKey()));
+        try {
+            context.startActivity(appIntent);
+        } catch (ActivityNotFoundException ex) {
+            context.startActivity(webIntent);
+        }
+    }
+
+    private void loadTrailerData(){
+        new FetchTrailerDataTask().execute();
+    }
+
+    public class FetchTrailerDataTask extends AsyncTask<Void, Void, ArrayList<Trailer>> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected ArrayList<Trailer> doInBackground(Void... params) {
+
+            URL videoRequestUrl = NetworkUtils.buildVideosUrl(mId);
+
+            try {
+                String jsonVideoResponse = NetworkUtils.getResponseFromHttpUrl(videoRequestUrl);
+                ArrayList<Trailer> trailers = MovieJsonUtils
+                        .extractTrailers(DetailActivity.this, jsonVideoResponse);
+                return trailers;
+
+            } catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<Trailer> trailers) {
+            if (trailers != null) {
+                mTrailerAdapter.setTrailers(trailers);
+            } else {
+                System.out.println("Didn't work");
+            }
+        }
+
+    }
+
 }
