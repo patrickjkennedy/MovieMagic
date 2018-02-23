6cebb3e1490ac91469b24ee609ac0bb311e59bd3
pkennedy89 <patrickken@gmail.com>
2018-02-23 16:16:29 +0000

    added Content URI to Contract

--- app/src/main/java/com/example/android/moviemagic/data/FavouriteContract.java
+++ app/src/main/java/com/example/android/moviemagic/data/FavouriteContract.java
@@ -1,5 +1,6 @@
 package com.example.android.moviemagic.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -8,7 +9,20 @@ import android.provider.BaseColumns;
 
 public class FavouriteContract {
 
+    // Define the authority
+    public static final String AUTHORITY = "com.example.android.moviemagic";
+
+    // The base content URI
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    // This is the path for the "favourites" directory
+    public static final String PATH_FAVOURITES = "favourites";
+
     public static final class FavouriteEntry implements BaseColumns {
+
+        // TaskEntry content URI = base content URI + path
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_FAVOURITES).build();
 
         // Table name
         public static final String TABLE_NAME = "favourites";
